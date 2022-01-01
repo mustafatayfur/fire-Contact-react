e7d38fd138626c80b3fb8c3bb849669030b8517e
mustafa <mltm0204@gmail.com>
2022-01-01 23:43:18 +0300

    ...

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,36 +1,34 @@
 import firebase from "./firebase";
-import { getDatabase, set, ref, push, onValue, query } from 'firebase/database'
-import { useEffect, useState } from "react";
-
-export const addInfo = (info)=>{
-    const db = getDatabase();
-    const userRef = ref(db, "contact")
-    const newUserRef = push(userRef)
-    set(newUserRef, {
-        username: info.username,
-        phoneNumber: info.phoneNumber,
-        gender: info.gender
+import { useState,useEffect } from "react";
+import { getDatabase,ref,push,set, onValue,query } from "firebase/database"
+export const addInfo=(info)=>{
+    const db=getDatabase();
+    const userRef=ref(db,"contact")
+    const newUserRef=push(userRef)
+    set(newUserRef,{
+        username:info.username,
+        phoneNumber:info.phoneNumber,
+        gender:info.gender
     })
     console.log("veri eklendi")
 }
-
-export const useFetch = ()=>{
-    const [contactList, setContactList] = useState()
-    const [isLoading, setIsLoading] = useState();
+export const useFetch=()=>{
+    const [contactList, setContactList] = useState();
+    const [isLoading,setIsLoading]=useState(false)
     useEffect(()=>{
-        setIsLoading(true)
-
-        const db = getDatabase();
-        const userRef = ref(db, 'contact');
-        onValue(query(userRef), snapshot => {
-          const contacts=snapshot.val()
-          // console.log(snapshot.val())
-          const contactArray = [];
-          for (let id in contacts) {
-            contactArray.push({ id, ...contacts[id] });
-          }
-          setContactList(contactArray)
-
-    }, [])
-    return{isLoading, contactList}
+      setIsLoading(true)
+      const db = getDatabase();
+      const userRef = ref(db, 'contact');
+      onValue(query(userRef), snapshot => {
+        const contacts=snapshot.val()
+        // console.log(snapshot.val())
+        const contactArray = [];
+        for (let id in contacts) {
+          contactArray.push({ id, ...contacts[id] });
+        }
+        setContactList(contactArray);
+        setIsLoading(false)
+      })
+    },[]);
+    return {isLoading,contactList};
 }
