1b167e28a2587aaf1a07a7d436fa35068d5a1633
mustafa <mltm0204@gmail.com>
2022-01-02 13:33:03 +0300

    env file is created

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDC3NO_EJdSImyOAV8kwDTPRTu-XnlqI8I",
-    authDomain: "fire-contact-app-8b62c.firebaseapp.com",
-    databaseURL: "https://fire-contact-app-8b62c-default-rtdb.firebaseio.com",
-    projectId: "fire-contact-app-8b62c",
-    storageBucket: "fire-contact-app-8b62c.appspot.com",
-    messagingSenderId: "117358710517",
-    appId: "1:117358710517:web:bd84860040b36c260bffbc"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
   };
 
   const firebase = initializeApp(firebaseConfig);
